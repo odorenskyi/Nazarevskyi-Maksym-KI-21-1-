ce48f5ca66f87ffb0b49f55367e618fc90fd9a18
Maksim Nazarevskyi <92650151+MaxNazarevskyi@users.noreply.github.com>
2022-03-22 13:21:22 +0200

    Added prj/ModulesNazarevskyi(Task_9-1)

---
+++ lab09/prj/ModulesNazarevskyi/ModulesNazarevskyi.h
@@ -0,0 +1,8 @@
+#ifndef MODULESNAZAREVSKYI_H_INCLUDED
+#define MODULESNAZAREVSKYI_H_INCLUDED
+
+#include <cmath>
+
+double s_calculation(float x, float y, float z);
+
+#endif // MODULESNAZAREVSKYI_H_INCLUDED
---
+++ lab09/prj/ModulesNazarevskyi/main.cpp
@@ -0,0 +1,55 @@
+#include "ModulesNazarevskyi.h"
+
+double s_calculation(float x, float y, float z)
+{
+    return z*sin(pow(x,2) * y) + sqrt(abs(z - x*12))/pow(y,3);
+}
+
+void bofort()
+{
+    switch ()
+    {
+    case 0:
+        cout << "< 0.3 - Штиль";
+        break;
+    case 1:
+        cout << "0.3 - 1.5 - Тихий";
+        break;
+    case 2:
+        cout << "1.5 - 3.3 - Легкий";
+        break;
+    case 3:
+        cout << "3.3 - 5.4 - Слабкий";
+        break;
+    case 4:
+        cout << "5.4 - 7.9 - Помірний";
+        break;
+    case 5:
+        cout << "7.9 - 10.7 - Свіжий";
+        break;
+    case 6:
+        cout << "10.7 - 13.8 - Сильний";
+        break;
+    case 7:
+        cout << "13.8 - 17.1 - Міцний";
+        break;
+    case 8:
+        cout << "17.1 - 20.7 - Дуже міцний";
+        break;
+    case 9:
+        cout << "20.7 - 24.4 - Шторм";
+        break;
+    case 10:
+        cout << "24.4 - 28.4 - Сильний шторм";
+        break;
+    case 11:
+        cout << "28.4 - 32.6 - Шквальний шторм";
+        break;
+    case 12:
+        cout << ">32.6 - Ураган(Буревій)";
+        break;
+    default:
+        cout << "Невірний бал Бофорта";
+    }
+}
+
