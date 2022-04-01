24c8ba47ebaf79c414df37d954645aad03b4efa3
Maksim Nazarevskyi <92650151+MaxNazarevskyi@users.noreply.github.com>
2022-04-01 14:33:57 +0300

    Bad Module

--- lab09/prj/ModulesNazarevskyi/ModulesNazarevskyi.h
+++
@@ -1,8 +0,0 @@
-#ifndef MODULESNAZAREVSKYI_H_INCLUDED
-#define MODULESNAZAREVSKYI_H_INCLUDED
-
-#include <cmath>
-
-double s_calculation(float x, float y, float z);
-
-#endif // MODULESNAZAREVSKYI_H_INCLUDED
