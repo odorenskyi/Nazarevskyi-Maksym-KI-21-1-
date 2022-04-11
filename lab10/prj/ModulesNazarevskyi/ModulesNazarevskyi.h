364b67787b968e542135e4ccee50f623fb985509
Maksim Nazarevskyi <92650151+MaxNazarevskyi@users.noreply.github.com>
2022-04-11 14:52:26 +0300

    Added Task_10-2

---
+++ lab10/prj/ModulesNazarevskyi/ModulesNazarevskyi.h
@@ -0,0 +1,26 @@
+#ifndef MODULESNAZAREVSKYI_H_INCLUDED
+#define MODULESNAZAREVSKYI_H_INCLUDED
+#include <iostream>
+
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+struct powers {
+int n;
+int m;
+};
+
+double s_calculation(float x, float y, float z);
+double bofort(int bal);
+powers powAndNum(int n, int m);
+int numBit(int num);
+void canToRedaction(string outputFileName, string inputFileName);
+bool inputTxtFile(string inputFileName, string inputText);
+bool myInfo(string outputFileName);
+int numberSymbolsInText(string outputFileName, string inputTextInFile);
+bool wordsInText(string outputFileName, string inputTextInFile);
+int numbersInText(string inputFileName, string inputTextInFile);
+bool timeInFile(string inputFileName);
+#endif // MODULESNAZAREVSKYI_H_INCLUDED
